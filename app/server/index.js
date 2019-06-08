f943fca264f52dc1cab4a460b11697c9f4e3bb38
Julio Assis <juliosevarollia@gmail.com>
2019-06-08 22:30:27 +0200

    implemented websocket server

--- app/server/index.js
+++ app/server/index.js
@@ -1,29 +1,65 @@
-import express from 'express';
+const express = require('express');
+const WebSocket = require('ws');
+const http = require('http');
 
-import serverRenderer from './middleware/renderer';
-
-const PORT = 3000;
+const PORT = 3001;
 const path = require('path');
 
 // initialize the application and create the routes
 const app = express();
 const router = express.Router();
 
-// root (/) should always serve our server rendered page
-router.use('^/$', serverRenderer);
+//initialize a simple http server
+const server = http.createServer(app);
 
 // other static resources should just be served as they are
 router.use(express.static(
   path.resolve(__dirname, '..', 'build'),
-  { maxAge: '30d'}
+  { maxAge: '30d' }
 ));
+
+app.get("/", (req, res) => {
+  res.send("connecting things blah");
+});
 
 // tell the app to use the above rules
 app.use(router);
 
+//initialize the WebSocket server instance
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+
+  //connection is up, let's add a simple simple event
+  ws.on('message', (message) => {
+    //log the received message and send it back to the client
+    console.log('received: %s', message);
+
+    const broadcastRegex = /^broadcast\:/;
+
+    if (broadcastRegex.test(message)) {
+      message = message.replace(broadcastRegex, '');
+
+      //send back the message to the other clients
+      wss.clients
+        .forEach(client => {
+          if (client != ws) {
+            client.send(`Hello, broadcast message -> ${message}`);
+          }
+        });
+
+    } else {
+      ws.send(`Hello, you sent -> ${message}`);
+    }
+  });
+
+  //send immediatly a feedback to the incoming connection    
+  ws.send('Hi there, I am a WebSocket server');
+});
+
 // start the app
-app.listen(PORT, (error) => {
-  if(error) {
+server.listen(PORT, (error) => {
+  if (error) {
     return console.log('something bad happened', error);
   }
 
